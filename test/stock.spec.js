8dedb0d242ff7da9d985074194bb18c2819635fe
Olga Talovikova and Rich Jardine <pair+olga+rich@allstate.com>
2016-06-17 10:50:35 -0500

    updated Stock

--- test/stock.spec.js
+++ test/stock.spec.js
@@ -3,15 +3,23 @@
 const expect = require('chai').expect;
 const Stock = require('../lib/stock');
 const nock = require('nock');
+const sinon = require('sinon');
+let clock;
 
 describe('Stock', () => {
   beforeEach(() => {
     // do mocking: if URL completely matching do redirect to a fake server.
+    clock = sinon.useFakeTimers();
     nock('http://dev.markitondemand.com')
       .get('/MODApis/Api/v2/Quote/json?symbol=AMZN')
-      .reply(200, { Status: 'SUCCESS', Name: 'Amazon', Symbol: 'AMZN', LastPrice: 100 });
+      .reply(200, { Status: 'SUCCESS', Name: 'Amazon', Symbol: 'AMZN',
+                    LastPrice: 100, Timestamp: 'Fri Jun 17 10:40:07 UTC-04:00 2016' });
   });
-
+  after(() => {
+    // clear http mock
+    clock.restore();
+    nock.restore();
+  });
 
   describe('constructor', () => {
     it('should construct a new Stock object', () => {
@@ -23,6 +31,7 @@ describe('Stock', () => {
 
   describe('#purchase', () => {
     it('should purchase stock', (done) => {
+      clock.tick(150);
       const stock1 = new Stock('AMZN');
       stock1.purchase(50, (err, totalPaid) => {
         expect(err).to.be.null;
@@ -30,7 +39,7 @@ describe('Stock', () => {
         expect(stock1.shares).to.equal(50);
         expect(stock1.name).to.equal('Amazon');
         expect(stock1.purchasePricePerShare).to.equal(100);
-        // expect(stock1.purchaseDate.getTime()).to.equal(222222);
+        expect(stock1.purchaseDate.getTime()).to.equal(150);
         expect(totalPaid).to.equal(5000);
         done();
       });
@@ -45,14 +54,24 @@ describe('Stock', () => {
 
       stock1.sell(30, (err, sharePrice, totalGained) => {
         expect(err).to.be.null;
-        expect(totalGained).to.be.above(0);
         expect(stock1.shares).to.equal(20);
-        expect(stock1.purchasePricePerShare).to.be.above(0);
-        expect(totalGained).to.equal(30 * sharePrice);
+        expect(sharePrice).to.equal(100);
+        expect(totalGained).to.equal(3000);
         done();
       });
+    });
+    it('should not sell stock', (done) => {
+      const stock1 = new Stock('AMZN');
+      stock1.shares = 50;
+      stock1.purchasePricePerShare = 100;
 
-      // });
+      stock1.sell(200, (err, sharePrice, totalGained) => {
+        expect(err).to.be.not.null;
+        expect(stock1.shares).to.equal(50);
+        expect(sharePrice).to.equal(0);
+        expect(totalGained).to.equal(0);
+        done();
+      });
     });
   });
 });
